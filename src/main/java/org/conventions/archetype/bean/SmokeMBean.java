e3d1be59221cec6064108fe114f5cec0ffabf378
rmpestano <rmpestano@gmail.com>
2014-06-03 23:54:24 -0300

    smokeTests initial dropin static analisys profile

---
+++ src/main/java/org/conventions/archetype/bean/SmokeMBean.java
@@ -0,0 +1,43 @@
+package org.conventions.archetype.bean;
+
+import org.apache.myfaces.extensions.cdi.core.api.scope.conversation.ViewAccessScoped;
+
+import javax.inject.Named;
+
+/**
+ * Created by rmpestano on 6/3/14.
+ *
+ * used for smokeTests
+ */
+@Named
+@ViewAccessScoped
+public class SmokeMBean {
+
+    private Boolean dbStatus;
+    private Boolean restEndpointStatus;
+
+
+    public void testBd(){
+        //todo
+    }
+
+    public void testRestEndpoint(){
+        todo
+    }
+
+    public Boolean getRestEndpointStatus() {
+        return restEndpointStatus;
+    }
+
+    public void setRestEndpointStatus(Boolean restEndpointStatus) {
+        this.restEndpointStatus = restEndpointStatus;
+    }
+
+    public Boolean getDbStatus() {
+        return dbStatus;
+    }
+
+    public void setDbStatus(Boolean dbStatus) {
+        this.dbStatus = dbStatus;
+    }
+}
