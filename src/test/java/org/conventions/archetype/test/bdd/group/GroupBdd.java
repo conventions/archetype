31c2c3388c5a63c7015b1a1a8f864ce8ff1f9fbf
rmpestano <rmpestano@gmail.com>
2014-07-25 10:31:29 -0300

    - updated arquillian  version & bdd whitebox tests

--- src/test/java/org/conventions/archetype/test/bdd/group/GroupBdd.java
+++ src/test/java/org/conventions/archetype/test/bdd/group/GroupBdd.java
@@ -6,6 +6,8 @@ import org.conventions.archetype.test.util.Deployments;
 import org.jboss.arquillian.container.test.api.Deployment;
 import org.jboss.arquillian.junit.Arquillian;
 import org.jboss.shrinkwrap.api.spec.WebArchive;
+import org.jboss.shrinkwrap.resolver.api.maven.Maven;
+import org.jboss.shrinkwrap.resolver.api.maven.MavenResolverSystem;
 import org.junit.runner.RunWith;
 
 /**
@@ -25,7 +27,12 @@ public class GroupBdd extends BaseBdd {
     {
         WebArchive archive = Deployments.getBaseDeployment()
                 .addPackage(BaseBdd.class.getPackage())
+                .addAsResource("org/conventions/archetype/test/bdd/group/group_bdd.story")
                 .addClass(GroupSteps.class);
+        
+        MavenResolverSystem resolver = Maven.resolver();
+        archive.addAsLibraries(resolver.loadPomFromFile("pom.xml").resolve("com.google.guava:guava:11.0.1").withTransitivity().asFile());
+        archive.addAsLibraries(resolver.loadPomFromFile("pom.xml").resolve("org.jbehave:jbehave-core:3.7.5").withTransitivity().asFile());
         return archive;
     }
 
